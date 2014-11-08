bba0b6faaa76ca315b2b5da6801e4197fff9dfc4
Bill Boyer <william.boyer.nyc@gmail.com>
2014-11-08 09:37:42 -0500

    Initial Commit

---
+++ ufora-demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ufora-demo
+//
+//  Created by Bill Boyer on 11/8/14.
+//  Copyright (c) 2014 Bill Boyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
